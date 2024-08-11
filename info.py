9536f3b451716be2cac80fe2ef9cf072a9e405f1
iopqk <alashrabha2000@gmail.com>
2024-08-11 17:11:20 +0530

    Update info.py

--- info.py
+++ info.py
@@ -143,7 +143,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "")
+URL = environ.get("URL", "https://indirect-lucienne-axombotz-d665751c.koyeb.app/")
 
 
 # Rename Info : If True Then Bot Rename File Else Not
